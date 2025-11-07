8b0e125408c158471c187fb236bea21b075732fb
Cai <13110818005@qq.com>
2025-11-07 22:18:30 +0800

    refactor: use WinAPI to get user inactive time

--- activity_monitor.py
+++ activity_monitor.py
@@ -1,54 +1,50 @@
 import time
 from datetime import datetime
-from pynput import mouse, keyboard
-
 from config import Config
 from light import Light
+import ctypes
+from ctypes import wintypes
 
 
 class ActivityMonitor:
     def __init__(self):
-        self.last_active: datetime = datetime.now()
+        self.inactivity_threshold = 0
         self.last_sync: datetime = datetime.now()
         self.light_on: bool = True
         Light.on()
-        self.auto_close_inactive: int = Config.instance.auto_close_inactive_seconds
-
-    @property
-    def inactivity_threshold(self) -> int:
-        return (datetime.now() - self.last_active).seconds
+        self.auto_close_inactive: int = Config.instance.auto_close_idle_timeout
 
     @property
     def sync_threshold(self) -> int:
         return (datetime.now() - self.last_sync).seconds
 
     def start_listening(self):
-        mouse_listener = mouse.Listener(
-            on_move=self._on_active,
-            on_click=self._on_active,
-            on_scroll=self._on_active
-        )
-        mouse_listener.daemon = True
-        mouse_listener.start()
-
-        keyboard_listener = keyboard.Listener(on_press=self._on_active)
-        keyboard_listener.daemon = True
-        keyboard_listener.start()
+        class LASTINPUTINFO(ctypes.Structure):
+            _fields_ = [('cbSize', wintypes.UINT), ('dwTime', wintypes.DWORD)]
 
         while True:
-            if self.inactivity_threshold > self.auto_close_inactive and self.light_on:
+            lastInputInfo = LASTINPUTINFO()
+            lastInputInfo.cbSize = ctypes.sizeof(lastInputInfo)
+            # noinspection PyUnresolvedReferences
+            ctypes.windll.user32.GetLastInputInfo(ctypes.byref(lastInputInfo))
+            # noinspection PyUnresolvedReferences
+            inactive_time = (ctypes.windll.kernel32.GetTickCount() - lastInputInfo.dwTime) // 1000
+            if inactive_time < self.inactivity_threshold:
+                self._on_active()
+            self.inactivity_threshold = inactive_time
+
+            if self.inactivity_threshold > self.auto_close_inactive and self.light_on and Config.instance.auto_close:
                 self.light_on = False
                 Light.off()
-            if self.sync_threshold > 30:
+
+            if self.sync_threshold > Config.instance.sync_status_interval:
                 self.last_sync = datetime.now()
                 self.light_on = Light.get_light_status()
                 print("Light sync successful:", self.light_on)
 
             time.sleep(1)
 
-    def _on_active(self, *args, **kwargs):
-        self.last_active = datetime.now()
-
-        if not self.light_on:
+    def _on_active(self):
+        if not self.light_on and Config.instance.auto_open:
             self.light_on = True
             Light.on()
